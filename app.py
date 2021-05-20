baf1b60b9198ea27ba45ecb98613a382b4022add
AlanGPT-93 <argarcia1693@gmail.com>
2021-05-19 23:09:35 -0500

    this is the final version of the project, using list as storage object. It works

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from utilis import database
 
-USER_CHOICE = """ Hi my dear user, I'll show you the menu:
+USER_CHOICE = """\n Hi my dear user, I'll show you the menu:
 
 - 'a' to add a new book
 - 'l' to list all books
@@ -17,20 +17,30 @@ def menu():
     user_choise = str(input(USER_CHOICE))
 
     while user_choise != "q":
+        
         if user_choise == "a":
             book_name = str(input("Please enter the Book's name: "))
             book_author = str(input("Please enter the Book's author: "))
             database.add_book(book_name, book_author)
             user_choise = str(input(USER_CHOICE))
+
         elif user_choise == "r":
             book_name = str(input("Please enter the Book's name: "))
             book_author = str(input("Please enter the Book's author: "))
             database.mark_as_read(book_name, book_author)
             user_choise = str(input(USER_CHOICE))
 
-
-    database.list_books()
-
-
+        elif user_choise == "d":
+            book_name = str(input("Please enter the Book's name: "))
+            book_author = str(input("Please enter the Book's author: "))
+            database.delete_book(book_name, book_author)
+            user_choise = str(input(USER_CHOICE))
+        
+        elif user_choise == "l":
+            database.list_books()
+            user_choise = str(input(USER_CHOICE))
+        
+        else:
+            user_choise = str(input(USER_CHOICE))
 
 menu()
--- utilis/database.py
+++ utilis/database.py
@@ -10,10 +10,12 @@ def add_book(book_name, book_author):
     books.append({"Name":book_name, "Author":book_author, "Read":False })
 
 def list_books():
-    print(books)
+    for i, book in enumerate(books):
+        print(f'\n {i+1}- {book["Name"]} by {book["Author"]}, read = {book["Read"]}' )
 
 def delete_book(book_name, book_author):
-    pass
+    global books
+    books = list(filter(lambda book: (book["Name"] == book_name and book["Author"] == book_author)  == False, books))
 
 def mark_as_read(book_name, book_author):
     for book in books:
